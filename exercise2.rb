9f681a19e33f31323e60c21365e0ca7bab715c5c
andrew-k512 <andrew.fiveonetwo@gmail.com>
2017-06-26 15:25:40 -0400

    finish exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,27 @@
+# Q1: How would you calculate a good tip for a 55 dolar meal?
+# Use puts to print the answer.
+# I would mutiply 55 * .1.
+
+puts "Answer 1: I mutiply the meal amount by .1 to get a tip of
+ten percent. So for a $55 meal the tip is $#{55 * 0.1}."
+
+# Q2 Try adding a string and an integer with the + operator.
+# What happens? Find a way to convert the integer into a string
+# first and use puts to print the result.
+
+puts "Answer 2: Gives an error, and the third line combines the string
+\"age\" with the integer 27 into one string which says age 27."
+
+puts "Age " + 27.to_s
+
+#Try outputting the result of 45628 multiplied by 7839 in a
+# sentence by using string interpolation.
+
+puts "Answer 3: 45628 multiplied by 7839 equals #{45628 * 7839}."
+
+# What's the value of the expression
+# (10 < 20 && 30 < 20) || !(10 == 11)?
+#Try figuring it out on your own before typing it in.
+
+puts "Answer 4: The answer is true, because the right side of the OR equation
+is true. "
