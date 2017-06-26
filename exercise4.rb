8f1cc3bb5b2680f01b07c4c116260a8dcd7a4cc5
andrew-k512 <andrew.fiveonetwo@gmail.com>
2017-06-26 17:29:35 -0400

    finish exercise 4

---
+++ exercise4.rb
@@ -0,0 +1,15 @@
+
+bitmaker = (1..100)
+
+bitmaker.each do |num|
+
+  if num % 3 == 0 && num % 5 == 0
+    puts "Bitmaker"
+  elsif num % 3 == 0
+    puts "Bit"
+  elsif num % 5 == 0
+    puts "Maker"
+  else
+    puts num
+end
+end
