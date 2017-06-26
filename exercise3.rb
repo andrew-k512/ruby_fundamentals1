33f34e83600749fbdea9937b0def2cf7b6c67f04
andrew-k512 <andrew.fiveonetwo@gmail.com>
2017-06-26 16:10:59 -0400

    finish exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,5 @@
-puts "What is your name? "
+puts "What is your name?"
+user_name = gets.chomp
+puts "Hello, #{user_name}! How old are you?"
+user_age = gets.chomp
+puts " #{2017 - user_age.to_i} was a good year!"
